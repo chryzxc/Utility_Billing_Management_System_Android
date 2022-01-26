4c633d504e8e5fb534fe70b5cbd5b4f615dd715c
Christian Rey Villablanca <chriztiann.villablanca@gmail.com>
2022-01-27 00:46:17 +0800

    commit

--- app/src/main/java/local/ubms/com/PaidBillAdapter.java
+++ app/src/main/java/local/ubms/com/PaidBillAdapter.java
@@ -2,16 +2,23 @@ package local.ubms.com;
 
 import android.content.Context;
 import android.content.Intent;
+import android.text.format.DateFormat;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.List;
+import java.util.Locale;
+import java.util.TimeZone;
 
 public class PaidBillAdapter extends RecyclerView.Adapter<PaidBillAdapter.ViewHolder> {
     private List<PaidBillList> myListList;
@@ -42,8 +49,24 @@ public class PaidBillAdapter extends RecyclerView.Adapter<PaidBillAdapter.ViewHo
     public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
         PaidBillList myList=myListList.get(position);
 
+        String dateStr = myList.getDate_paid();
+        SimpleDateFormat df = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss", Locale.ENGLISH);
+        df.setTimeZone(TimeZone.getTimeZone("UTC"));
+        Date date = null;
+        try {
+            date = df.parse(dateStr);
+            df.setTimeZone(TimeZone.getDefault());
+
+          //  String formattedDate = df.format(date);
+          //  Toast.makeText(ct, date.toString(), Toast.LENGTH_SHORT).show();
+            holder.paidBillDate.setText("Date paid: "+DateFormat.format("MM-dd-yyyy hh:mm:ss a",date));
+        } catch (ParseException e) {
+            Toast.makeText(ct, e.getMessage().toString(), Toast.LENGTH_SHORT).show();
+        }
+
+
         holder.paidBillAmount.setText("Amount: ₱"+myList.getAmount_tendered());
-        holder.paidBillDate.setText("Date paid: "+myList.getDate_paid());
+      //  holder.paidBillDate.setText("Date paid: "+myList.getDate_paid());
         holder.paidBillNumber.setText("Bill #"+myList.getBill_id());
         holder.paidBillMethodText.setText(myList.getPayment_method());
 
